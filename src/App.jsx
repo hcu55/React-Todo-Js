7fc2166acb243fb7b497fdf8c880a3b3c4e19299
ChanUi <hcu55@naver.com>
2024-07-24 11:32:43 +0900

    :sparkles: Todo 조회 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import TodoHeader from './components/todos/TodoHeader'
 import TodoBody from './components/todos/TodoBody'
 import DefaultLayout from './layouts/DefaultLayout'
+import { useState } from 'react'
+
+const dummyTodos = [
+  {
+    id: 1,
+    title: 'React 공부',
+    summary: 'React를 공부한다.',
+    category: 'TODO',
+  },
+  {
+    id: 2,
+    title: '점심 먹기',
+    summary: '점심을 먹는다.',
+    category: 'PROGRESS',
+  },
+  {
+    id: 3,
+    title: '커피 마시기',
+    summary: '커피를 마신다.',
+    category: 'DONE',
+  }
+]
 
 function App() {
+
+  const [todos, setTodos] = useState(dummyTodos);
 
   return (
     <>
@@ -16,7 +40,7 @@ function App() {
         </header>
         <section className="max-w-xl m-4 mx-auto">
           <TodoHeader />
-          <TodoBody />
+          <TodoBody  todos={todos}/>
         </section>
       </DefaultLayout>
     </>
